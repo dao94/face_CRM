a7134e8fbbace0dbb8afad1087efecdf408f3d30
thinhit <thinhit@yahoo.com>
2015-06-12 23:53:05 +0700

    update models and contronller structure

---
+++ api/models/Pages.js
@@ -0,0 +1,14 @@
+/**
+* Pages.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	page_name: {type: 'string', required: true}
+  }
+};
+
