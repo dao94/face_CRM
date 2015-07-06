891c5a8493d2b7e5a22d4d7154ee6c17c04150f5
thinhit <thinhit@yahoo.com>
2015-07-06 21:35:47 +0700

    fix access_token

--- html/js/controllers/LoginController.js
+++ html/js/controllers/LoginController.js
@@ -3,8 +3,4 @@ angular.module('fCRM')
 		$scope.login = function (){
 			window.location = "http://www.facebook.com/dialog/oauth/?scope=" + fb_sdk.permission + "&client_id="+ fb_sdk.appId +"&redirect_uri=http://face.local.com/html&response_type=code";
 		}
-		
-		$restful.post('messages/getMessage',{username: 'idsweb.vn'},function (err,resp) {
-                      console.log(resp);
-        });
 	}])
