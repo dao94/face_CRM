a11ab827bcdd498a1bd4861293c7ec09abe18fb2
thinhit <thinhit@yahoo.com>
2015-07-17 22:51:14 +0700

    update

--- html/js/controllers/inboxs/InboxsController.js
+++ html/js/controllers/inboxs/InboxsController.js
@@ -1,16 +1,35 @@
 angular.module('fCRM')
 	.controller('InboxsController', ['$scope', '$state', '$stateParams', '$restful', function ($scope, $state, $stateParams, $restful){
-		$scope.list_conversation = [];
-		$restful.post('messages/getMessage',{username: $stateParams.page},function (err,resp) {
-			console.log(resp);
-        });
+		$scope.list_conversation   = [];
+		$scope.conversationLoading = true;
+		console.log('hello world');
+
+		
+		
+
+		$scope.syncMessage = function (callback){
+			$restful.post('messages/getMessage',{username: $stateParams.page},function (err,resp) {
+				return (callback && typeof callback == 'function ') ? callback() : false;
+        	});
+		}
 
 
         $scope.getConversation = function (){
         	$restful.get('conversations/show',{username: $stateParams.page},function (err,resp) {
-				$scope.list_conversation = resp.data;
+				$scope.conversationLoading = false;
+				$scope.list_conversation   = resp.data;
+				$state.transitionTo('app.inboxs', {page: $stateParams.page, conversationId: resp.data[0].id})
         	});
         }
 
-        $scope.getConversation();
+        
+        if(!$stateParams.conversationId){
+			$scope.syncMessage();
+			$scope.getConversation();
+		}else {
+			if($scope.list_conversation.length == 0){
+				$scope.syncMessage();
+				$scope.getConversation();
+			}
+		}
 	}])
